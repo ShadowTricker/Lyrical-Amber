a70010269b35a56d2ed291f046274f0714bb323d
zhiyu.c.wang <zhiyu.c.wang@accenture.com>
2019-12-20 09:31:02 +0800

    [modify] modify diff funcs

--- src/components/diff-function/diff.ts
+++ src/components/diff-function/diff.ts
@@ -100,9 +100,9 @@ const diffNew = (preValue, curValue, isRoot: boolean, container: any = {}, paren
             keys.forEach(key => {
                 const { isEqual, isEnd } = diffNew(preValue[key], curValue[key], false, container);
                 if (isEnd && !isEqual) {
-                    container[key] = diffNew(preValue[key], curValue[key], false, container, [ ...parents, key ]);
+                    container[key] = diffNew(preValue[key], curValue[key], false, container, [...parents, key]);
                 } else if (!isEnd) {
-                    diffNew(preValue[key], curValue[key], false, container, [ ...parents, key ] );
+                    diffNew(preValue[key], curValue[key], false, container, [...parents, key]);
                 }
             });
         }
@@ -110,7 +110,7 @@ const diffNew = (preValue, curValue, isRoot: boolean, container: any = {}, paren
     }
 };
 
-function getType(value) {
+function getType(value: any): string {
     if (Number.isNaN(value)) {
         // or value !== value
         return 'NaN';
@@ -123,7 +123,7 @@ function getType(value) {
     }
 }
 
-console.log(
+/* console.log(
     diffNew(
         { 
             super: [
@@ -147,4 +147,92 @@ console.log(
         [1, 2, 4],
         true
     )
+); */
+
+const diffNewArray = (preValue, curValue, parents = [], type: string = null, container: any[] = []) => {
+
+    // get the two elements type
+    const preType = getType(preValue);
+    const curType = getType(curValue);
+    const mergeType = preType === curType ? preType : null;
+
+    if (!mergeType) {
+        return {
+            preValue,
+            curValue,
+            type: type ? type : null,
+            key: parents[parents.length - 1] ? parents[parents.length - 1] : null,
+            parents: parents.slice(0, -1)
+        };
+    }
+
+    if (mergeType !== 'symbol' && mergeType !== 'object' && mergeType !== 'function' && mergeType !== 'array') {
+        if (JSON.stringify(preValue) !== JSON.stringify(curValue)) {
+            return {
+                preValue,
+                curValue,
+                type: type ? type : null,
+                key: parents[parents.length - 1] ? parents[parents.length - 1] : null,
+                parents: parents.slice(0, -1)
+            };
+        }
+    }
+
+    if (mergeType === 'symbol' || mergeType === 'function') {
+        if (preValue !== curValue) {
+            return {
+                preValue,
+                curValue,
+                type: type ? type : null,
+                key: parents[parents.length - 1] ? parents[parents.length - 1] : null,
+                parents: parents.slice(0, -1)
+            };
+        }
+    }
+
+    if (mergeType === 'object' || mergeType === 'array') {
+
+        // get total keys about the two elements, and delete the repeat keys.
+        const keys = new Set([
+            ...Object.keys(preValue ? preValue : {}),
+            ...Object.keys(curValue ? curValue : {})
+        ]);
+        // console.log(keys);
+        if (keys.size > 0) {
+            keys.forEach(key => {
+                const result = diffNewArray(preValue[key], curValue[key], [...parents, key], getType(curValue || preValue));
+                if (result) {
+                    if (getType(result) === 'array') {
+                        container.push(...result as any[]);
+                    } else {
+                        container.push(result);
+                    }
+                }
+            });
+        }
+        return container;
+    }
+};
+
+console.log(
+    diffNewArray(
+        { 
+            super: [
+                { sub: 1 },
+                { sub: 2, sub2: 3 },
+                { sub: 2, sub2: 3 },
+            ]
+        },
+        {
+            super: [
+                { sub: 2 },
+                { sub: 1 },
+                { sub: 1, sub2: 5 },
+            ]
+        }
+    )
 );
+/* console.log(diffNewArray(
+    [1, 2, 3],
+    [1, 2, 4]
+)); */
